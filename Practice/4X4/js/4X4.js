f6d9916d994cebba29ab5367875fa82c8e5ea356
das7945 <das7945@naver.com>
2022-12-17 17:48:24 +0900

    과제

--- Practice/4X4/js/4X4.js
+++ Practice/4X4/js/4X4.js
@@ -1,13 +1,24 @@
 const block = document.querySelectorAll(".block");
-const options = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+// const options = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
 const startBtn = document.querySelector("#startBtn");
 
 
 let num = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 let win = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0'
 
+
+[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0]
+
 let plus = [3, 7, 11, 15]
 let minus = [0, 4, 8, 12]
+
+
+function ran() { // 1~15
+    return (Math.floor(Math.random()*15)+1);
+}
+
+
+
 
 // let currentPlayer
 let running = false;
@@ -35,8 +46,7 @@ function randomNum() {
         }
     }
     document.querySelectorAll(".block").forEach((value, index) => value.innerText = num[index])
-    document.querySelectorAll(".block").forEach((value, index) => value.addEventListener("click", click))
-    
+    document.querySelectorAll(".block").forEach((value, index) => value.addEventListener("click", click))    
 }
 
 function click() {
@@ -54,11 +64,12 @@ function click() {
         num[i] = 0;
         console.log("+4")
     }else if(num[i-4] == 0) {
-        num[i-4] = num[i];
+              num[i-4] = num[i];
         num[i] = 0;
         console.log("-4")
     }
-    document.querySelectorAll(".block").forEach((value, index) => value.innerText = num[index]) // 배열 정보랑 버튼이랑 매칭
+    document.querySelectorAll(".block").forEach((value, index) => value.innerText = num[index]) 
+    // 배열 정보랑 버튼이랑 매칭
     
     if(num.toString() == win) {
         setTimeout(() => {
@@ -69,9 +80,7 @@ function click() {
     }
 }
 
-function ran() { // 1~15
-    return (Math.floor(Math.random()*15)+1);
-}
+
 
 function startGame() {
     randomNum();
--- Practice/weather/weather.js
+++ Practice/weather/weather.js
@@ -14,8 +14,7 @@ app.get('/weather', (req, res) => {
     {day:4, location:'지역명4', temp:'온도4', weather: '안개'},
     {day:5, location:'지역명5', temp:'온도5', weather: '흐림'}
   ];
-  // 배열 객체를 JSON 문자열로 변환하여 클라이언트에게 보낸다.
-  // serialization(직렬화)
+
   res.send(JSON.stringify(arr));
 });
 
@@ -23,15 +22,17 @@ app.get('/proxy', (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
   res.set('Content-Type', 'application/json; charset=UTF-8');
 
-  let openApiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?" +
-    "serviceKey=DbSZXR9hJZpE84BWY2zIwk2rohohciUQnK0WNVgFTxPdXiiSIWZ4X6buqhUdoenXqSYvJXiuNMP77pq4NGwFiQ%3D%3D" +
-    "&pageNo=1" +
-    "&numOfRows=1000" +
-    "&dataType=JSON" +
-    "&base_date=" + req.query.base_date +
-    "&base_time=0600" +
-    "&nx=" + req.query.nx +
-    "&ny=" + req.query.ny +
+  let openApiUrl = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?" +
+  "serviceKey=B1owd940AsmBpdnx%2FoLXYOpKox42TuSDluVY%2BtcxexysxKF5K8LOjbfDD%2BqVn2Z1zdWMtAsHKC34a8lHN2Q4LQ%3D%3D" +
+  "&pageNo=1" +
+  "&numOfRows=100" +
+  "&dataType=JSON" +
+  "&base_date=" + req.query.base_date +
+  "&base_time=0500" +
+  "&nx=" + req.query.nx +
+  "&ny=" + req.query.ny +
+
+
 
   request.get({
       url: openApiUrl
@@ -49,3 +50,55 @@ app.listen(
 );
 console.log("서버 시작했음!");
 
+
+
+let today = new Date();   // Sat Dec 17 2022 15:37:33 GMT+0900 (한국 표준시)
+
+let year = today.getFullYear(); // 년도
+let month = today.getMonth() + 1;  // 월. 1월은 0으로 표현되므로 +1
+let date = today.getDate();  // 날짜let day = today.getDay();  // 요일
+
+let today = new Date();  
+document.write(today.toLocaleDateString() + '<br>'); //년.월.일
+document.write(today.toLocaleTimeString() + '<br>'); //오전오후. 시.분.초
+document.write(today.toLocaleString() + '<br><br>'); //년.월.일. 오전오후. 시.분.초
+
+
+
+
+
+
+// 현재 위치 정보를 가져오기 위한 함수
+// 함수를 사용할 경우 앞에 async function <<<와 같이 붙여줘야함
+// promise
+async function getLocation() {  //비동기
+  let XY = new Object();
+  if(navigator.geolocation) {
+    let promise = new Promise((resolve, rejected) => {
+      navigator.geolocation.getCurrentPosition((position) => { 
+        resolve(position);
+      });//getCurrentPosition 비동기를 동기로 변경하는 작업
+    });
+
+    let position = await promise;
+    // navigator.geolocation.getCurrentPosition(function(position){
+      XY.lat = position.coords.latitude;  // 위도
+      XY.lng = position.coords.longitude; // 경도   
+  }
+  return xy;
+}
+
+
+// let local = new Date();
+
+// let time = local.toLocaleString("ko-kr"); //년.월.일. 오전오후. 시.분.초
+// console.log("time");
+
+
+const date = new Date();
+
+const year = date.getFullYear();
+const month = ('0' + (date.getMonth() + 1)).slice(-2);
+const day = ('0' + date.getDate()).slice(-2);
+const dateStr = year + '-' + month + '-' + day;
+console.log("dateStr");
